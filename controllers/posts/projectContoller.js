93d638889cbe96e880cedf4c0d0b6993d12d71e6
matheusAznavour <matheus.lins896@gmail.com>
2025-11-19 10:43:29 -0300

    seting up multer and cloudinary done.

--- controllers/posts/projectContoller.js
+++ controllers/posts/projectContoller.js
@@ -1,10 +1,17 @@
+const cloudinary = require("./../../middleware/config/cloudinaryConfig");
+
 function createProject(req, res){
     res.render("posts/project/create");
 }
 
 function createProjectForm(req, res){
-
-}
+    cloudinary.uploader.upload(req.file.path, (err, result)=>{
+        if(err){
+            return console.log(err);
+        };
+        console.log(result);
+    });
+};
 
 module.exports = {
     createProject, createProjectForm,
--- routes/posts/projectRouter.js
+++ routes/posts/projectRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("./../../middleware/multer");
 const projectController = require("./../../controllers/posts/projectContoller");
 
 router.get("/project/:id/:slug", ()=>{});
 
 router.get("/project/create", projectController.createProject);
-router.post("/project/create", projectController.createProjectForm);
+router.post("/project/create", upload.single("image"), projectController.createProjectForm);
 
 router.get("/project/:id/:slug/edit", ()=>{});
 router.post("/project/:id/:slug/edit", ()=>{});
