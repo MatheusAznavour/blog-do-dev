58b1e308e03e483c602bcffc679f8277dcd00672
matheusAznavour <matheus.lins896@gmail.com>
2025-11-20 18:26:14 -0300

    error popup in profile view done.

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,6 +4,9 @@ async function profile(req, res){
     const { id, username } = req.params;
     
     const profile = await profileService.getProfile(id);
+    if(profile.length === 0){
+        return res.render("profile/home", {error: ["Profile not found!"]});
+    }
 
     console.log(profile)
 
