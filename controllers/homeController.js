111483950ea468aeea849e139089eb548888dffe
matheusAznavour <matheus.lins896@gmail.com>
2025-11-19 22:28:06 -0300

    logic for article retrievement done.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,14 +6,17 @@ async function home(req, res) {
     const offset = parseInt(req.query.offset || 0);
     const articles = await homeService.getArticle(limit, offset);
     if(postType == "article"){
-        console.log("ITS EQUALL")
+        if(articles.length === 0){
+            return res.render("home", {error: ["Could not find any post"]})
+        }
+        return res.render("home", {articles});
     }
-    if(articles.length === 0){
-        return res.render("home", {error: ["Could not find any post"]});
+    if(postType == "project"){
+        console.log("EEE DESSE")
+        return res.render("home");
     }
 
     //console.log(article, limit, offset)
-    res.render("home", {articles});
 };
 
 module.exports = { home };
